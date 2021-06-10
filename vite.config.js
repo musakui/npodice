a2dd5bda22f7f1d4fce5dc74a749b3d2aef9c052
Shun Git <kwokshungit@gmail.com>
2021-06-10 20:01:54 +0800

    add bundle size visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 
+import { visualizer } from 'rollup-plugin-visualizer'
+const vis = visualizer({
+  json: true,
+  brotliSize: true,
+  filename: 'stats/stats.json',
+})
+
 export default defineConfig({
   base: '/npodice/',
   build: {
     chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      plugins: [
+        vis,
+      ],
+    },
   },
 })
